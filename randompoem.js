05d08de9db2f75cf6f40e5193c35133ce4050b07
Roland <rolanddeboo@gmail.com>
2013-06-24 18:00:34 +0000

    fixed indentation

--- randompoem.js
+++ randompoem.js
@@ -4,15 +4,15 @@ var cheerio = require('cheerio');
 var poemid = 15717;
 var url = 'http://www.poets.org/viewmedia.php/prmMID/' + poemid;
 
-    request(url, function(err, resp, body) {
-        if (err)
-            throw err;
-        $ = cheerio.load(body);
+request(url, function(err, resp, body) {
+    if (err)
+        throw err;
+    $ = cheerio.load(body);
 
-var title = $('h1').text();
-var poem = $('h1').parent().parent().next().next().next().next().text().trim();
+    var title = $('h1').text();
+    var poem = $('h1').parent().parent().next().next().next().next().text().trim();
 
-console.log(url);
-console.log(title);
-console.log(poem);        
-    });
+    //console.log(url);
+    console.log(title);
+    console.log(poem);        
+});
